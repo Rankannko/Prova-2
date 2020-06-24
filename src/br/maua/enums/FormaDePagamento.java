fb43af7b47a87a1cda1613d896d063a2e7b0d2bb
Rankannko <azevedomasterjp27@hotmail.com>
2020-06-24 16:46:38 -0300

    Enum Stats
    Emumeração do status, e criação da classe Menu

--- src/br/maua/enums/FormaDePagamento.java
+++ src/br/maua/enums/FormaDePagamento.java
@@ -1,4 +1,5 @@
 package br.maua.enums;
 
 public enum FormaDePagamento {
+    DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO
 }
---
+++ src/br/maua/enums/Status.java
@@ -0,0 +1,5 @@
+package br.maua.enums;
+
+public enum Status {
+    REALIZADO,PREPARACAO,SAIU_PARA_ENTREGA,ENTREGUE,DEVOLVIDO
+}
