e880ff081b27e1f94273c31105ad0f514e6e6e79
Rankannko <azevedomasterjp27@hotmail.com>
2020-06-26 13:48:34 -0300

    Main Java Docs
    Criação do Java Docs na Main

--- src/br/maua/Main.java
+++ src/br/maua/Main.java
@@ -1,9 +1,12 @@
 package br.maua;
 import br.maua.classes.Menu;
-import br.maua.classes.Usuario;
 
+/**
+ * Classe inicial, chama o programa, importando a classe menu. Deixado separadamente para testes durante a implementação e refatorações futuras
+ * @Autor João Pedro de Pauda Santoro Azevedo RA: 18.02277-4
+ * e-mai: azevedomasterjp27@hotmail.com
+ */
 public class Main {
-
     public static void main(String[] args) {
     Menu.Run();
     }
